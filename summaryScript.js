9c19e426e59093935bcd2acdaf0d4180ff884387
thedenatured <supamonkey7@hotmail.com>
2016-07-25 22:33:04 +0100

    Update summaryScript.js

--- summaryScript.js
+++ summaryScript.js
@@ -1,127 +1,153 @@
-/* step 1/2 create a "var" for your new article, put it above the first var. use the existing as an example the name can be anything as long as it is unique
-you'll have to remember it for when you create the actual page that the article is on. the name goes after var for example
-var name = { etc...
-title:          the title is how it shows over the summary boxes at the bottom of the page. the <br /> is what seperate the 1st and 2nd line
-image:          image is the adress of the perfectly square image you want for the summary boxes
-link:           this is the adress of the article page
-describe:       make this about the same length as the rest. if it doesn't take up as many lines as the others in situ it messes with the layout
-                (puts a white box arround your summary box) and you'll have to adjust the describe here to get it right.
-
-step 2 is at the bottom of the page
-*/
+var smell ={
+	heading: "A smell is worth a thousand words",
+	title2: "A smell is worth",
+	title3: "A thousand words",
+	image: "images/smell.jpg",
+	image2: "images/smell2.jpg",
+	link: "articles/smell.html",
+	describe: "Some animals produce their own natural pererfume.",
+	type: "article",
+	author: "john",
+};
+var chilli ={
+	heading: "The unlikely winner",
+	title2: "The unlikely",
+	title3: "Winner",
+	image: "images/chilli.jpg",
+	image2: "images/chilli2.jpg",
+	link: "articles/chilli.html",
+	describe: "You couldn't beat a chicken in this competition.",
+	type: "article",
+	author: "john",
+};
+var frack ={
+	heading: "What the frack?",
+	title2: "What the",
+	title3: "Frack?",
+	image: "images/frack.jpg",
+	image2: "images/frack2.jpg",
+	link: "articles/frack.html",
+	describe: "What the frack is fracking?",
+	type: "article",
+	author: "holly",
+};
+var hipster ={
+	heading: "Hipster animals",
+	title2: "Hipster",
+	title3: "Animals",
+	image: "images/kermode.jpg",
+	image2: "images/kermode2.jpg",
+	link: "articles/hipster.html",
+	describe: "Some animals choose their own unique style.",
+	type: "video",
+	author: "david",
+};
+var fast ={
+	heading: "Blink and you'll miss it",
+	title2: "Blink and",
+	title3: "you'll miss it",
+	image: "images/fast.jpg",
+	image2: "images/fast2.jpg",
+	link: "articles/fast.html",
+	describe: "Cheetah's are pretty fast, but who's faster?",
+	type: "article",
+	author: "john",
+};
 var pointlessOrgans = {
     heading: "Pointless Organs",
-    title:"Pointless<br>Organs",
-    image: "http://thedenatured.com/images/summary/pointless.jpg",
+    title2:"Pointless",
+    title3:"Organs",
+    image: "images/pointless.jpg",
+    image2:"images/pointless2.jpg",
     link: "/videos/pointlessOrgans.html",
-    describe: "Your body is a finely honed machine, evolution has shaped you to perfection right? Actually evolution is a tad lazy so you've got some spare bits. Like the appendix, what even is the point?",
-    type: "video"
+    describe: "Evolution is a tad lazy so you've got some spare bits.",
+    type: "video",
+    author: "david"
     };
 
 var hitchhiker = {
     heading: "The Microscopic Hitchhiker",
-    title: "The Microscopic Hitchhiker",
-    image: "http://thedenatured.com/images/summary/Hitchhiker_square.jpg",
+    title2: "The Microscopic",
+    title3: "Hitchhiker",
+    image: "images/pointless.jpg",
+    image2:"/images/.jpg",
     link: "http://thedenatured.com/articles/hitchhiker.html",
-    describe: "Think of a hitchhiker and you might conjure up images of a group of carefree students on a jail break, Jim Morrison of The Doors or maybe even, for the biologists out there...",
-    type: "article",
-};
-
-var smell = {
-    heading: "A Smell Is Worth A Thousand Words",
-    title: "A Smell Is Worth <br />A Thousand Words",
-    image: "http://thedenatured.com/images/summary/darkEyeJunco.jpg",
-    link: "http://thedenatured.com/articles/aSmell.html",
-    describe: "We humans like to smell good, using perfumes and colognes to try and give us an edge. Smell is used to win over a partner throughout the animal kingdom, albeit in a different fashion.",
+    describe: "Think of a hitchhiker but really really small",
     type: "article",
 };
 
 var sexyMen = {
     heading:"Sexy Men",
-    title: "Sexy<br />Men",
-    image: "http://thedenatured.com/images/summary/peacock2.jpg",
+    title2: "Sexy",
+    title3: "Men",
+    image: "images/pointless.jpg",
+    image2:"images/.jpg",
     link: "http://thedenatured.com/videos/sexyMen.html",
-    describe: "You want to know why men are so damn sexy? Well over here at The Denatured we know a thing or two about sexy men, so let us be your guide in this erotic part of science.",
+    describe: "Do you want to know why men are so damn sexy?",
     type: "video"
 };
 
-var chilli = {
-    title: "The Unlikely<br />Winner",
-    image: "http://thedenatured.com/images/summary/rooster.jpg",
-    link: "http://thedenatured.com/articles/theUnlikelyWinner.html",
-    describe: "You will beat your everyday chicken in most competitions even if you have to get inventive. There is, however, one competition in which you will meet your match.",
-    type: "article"
-};
 var zombie = {
-    title: "",
-    image: "http://thedenatured.com/images/summary/zombie.bmp",
+    title2: "Real",
+    title3: "Zombies",
+    image: "images/pointless.jpg",
+    image2:"images/.jpg",
     link: "http://thedenatured.com/videos/zombie.html",
-    describe: "It's Halloween so we've gone slightly off of the beaten track of science. We're talking about how Zombies could be real. Thanks to Our beautiful new editor: Lightning Storr",
+    describe: "Could there ever be real zombies? We check the science.",
     type: "video"
 };
 var dying = {
-    title: "Dying? <br /> What For",
-    image: "http://thedenatured.com/images/summary/skullMeSq.jpg",
+    title2: "Dying?",
+    title3: "What For",
+    image: "images/pointless.jpg",
+    image2:"images/.jpg",
     link: "http://thedenatured.com/videos/whyDie.html",
-    describe: "Why do we grow old and die? It might seem like ageing isn't a good adaptation but as ever evolution has the answer. And as ever we're going to have a pop at explaining it for you. Enjoy.",
+    describe: "Why do we grow old and die? It might seem like a bad idea.",
     type: "video"
 };
 var teach = {
-    title: "Teach<br /> The Kids",
-    image: "http://thedenatured.com/images/summary/blackBoard.jpg",
+    title2: "Teach",
+    title3: "The Kids",
+    image: "images/pointless.jpg",
+    image2:"images/.jpg",
     link: "http://thedenatured.com/articles/teach1.html",
-    describe: "What gets you excited about science? Is it the smell of freshly poured agar in the morning? What gets teenagers excited about science? Apparently nothing.",
+    describe: "What gets you excited about science? Apparently nothing.",
     type: "article"
 };
 var adder = {
-    title: "European<br />Adder",
-    image: "http://thedenatured.com/images/summary/adderSQ.jpg",
+    title2: "European",
+    title3: "Adder",
+    image: "images/adderSQ.jpg",
+    image2:"images/adder.jpg",
     link: "http://thedenatured.com/videos/adder.html",
-    describe: "The enigmatic European Adder is one of the hardiest and widespread snakes on earth, and is Britain's only venomous snake. Unfortunately, it is suffering declines due to ...",
+    describe: "The European Adder is Britain's only venemous snake.",
     type: "video"
 };
-var frack = {
-    title: "What the<br />Frack?",
-    image: "http://thedenatured.com/images/summary/fracking.jpg",
-    link: "http://thedenatured.com/articles/whatTheFrack.html",
-    describe: "As protestors gather this week against the test oil drilling ('fracking') we're led to question what the frack are they going on about and why is this such an important environmental issue?",
-    type: "article"
-};
 var fittest = {
-    title: 'Survival of<br />The "Fittests"',
-    image: "http://thedenatured.com/images/summary/sqBadgerChimp.jpg",
+    title2: 'Survival of',
+    title3: 'The "Fittest"',
+    image: "images/pointless.jpg",
+    image2:"images/.jpg",
     link: "http://thedenatured.com/videos/survivalOfTheFittest.html",
-    describe: "Selection doesn't make individuals better. I have a go at making the aim of evolution a bit clearer for folks and have a chatter about the concept of selfishness.",
+    describe: "Selection doesn't make you better. It makes you selfish.",
     type: "video"
 };
 var blink = {
-    title: "Blink and<br />You'll Miss It",
-    image: "http://thedenatured.com/images/summary/falcon.jpg",
+    title2: "Blink and",
+    title3: "You'll Miss It",
+    image: "images/fast.jpg",
+    image2:"images/fast2.jpg",
     link: "http://thedenatured.com/articles/blinkAnd.html",
-    describe: "He's been stalking his prey for hours and it's all been boiling down to this moment. He crouches down and tenses his muscles, ready to attack and claim his prize. The chase begins.",
+    describe: "Cheetah's are pretty fast but who's faster?",
     type: "article"
 };
 var prettyBird = {
-    title: "Pretty Bird<br />Ugly Friends",
-    image: "http://thedenatured.com/images/summary/housefinch.jpg",
+    title2: "Pretty Bird",
+    title3: "Ugly Friends",
+    image: "images/housefinch.jpg",
+    image2:"images/housefinch2.jpg",
     link: "http://thedenatured.com/videos/prettyBirdUglyFriends.html",
-    describe: "Male finches may pick and choose their social group to maximise their chance of mating. They will leave a group of pretty males in the hopes of finding uglier friends.",
+    describe: "This bird just wants to have ugly friends.",
     type: "video"
 };
-var hipster = {
-    title: "Hipster<br />Animals",
-    image: "http://thedenatured.com/images/summary/kermode.jpg",
-    link: "http://thedenatured.com/videos/hipsterAnimals.html",
-    describe: "Most animals tend to be quite similar to each other with in a species but some animals choose their own style. This video looks at how unusual individuals cope with life.",
-    type: "video"
-};
-var rhea = {
-    title: "Rhea:<br />Artist",
-    image: "/images/authorRhea.jpg",
-    link: "",
-    describe: "A MASSIVE thanks to Rhea 'Lightning-Storr' who we've press ganged into giving up her  artisty time to edit our videos for us. I really couldn't make these without her.",
-    type: "article"
-};
-/*step 2/2 put the name you used in the var above at the start of this list and seperate it with a comma.
-*/
-var summaries = [pointlessOrgans, hitchhiker, smell, sexyMen, chilli, zombie, dying, teach, adder, frack, fittest, blink, prettyBird, hipster, rhea];
+var summaries = [chilli, smell, pointlessOrgans, hitchhiker, adder, sexyMen, zombie, dying, teach, frack, fittest, fast, prettyBird, hipster];
