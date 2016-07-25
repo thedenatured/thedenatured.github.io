9d6ae226716d28e516cfef287d0b0011461f4f49
thedenatured <supamonkey7@hotmail.com>
2016-07-25 22:33:48 +0100

    Create authors.js

---
+++ authors.js
@@ -0,0 +1,17 @@
+var holly = {
+    name: "Youlden",
+    portrait: "images/authorHolly.jpg",
+    details: "Renowned for her talents in tree-identification and list-making. Having dabbled in Science Journalism and now full-time educator, she hopes to teach the world the joys of the natural world or, failing that, the waggle dance.",
+    };
+
+var david = {
+    name: "Wells",
+    portrait: "images/authorDavid.jpg",
+    details: "Powered by a passion for Evolution and a pair of extraordinary eyebrows, Wells is both our Presenter and Website pro. His love of the slightly inappropriate has lead him to research incest in the Banded Mongoose.",
+    };
+    
+var john = {
+    name: "Wilde",
+    portrait: "images/authorJohn.jpg",
+    details: "TheDenatured's own king in the north is currently gracing the capital with his Cumbrian charms. He is working in TV which apparently involves a lot of YouTube, for 'research'.",
+    };
